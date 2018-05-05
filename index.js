0bd807b641c5933109ad89f37a933d83ab420e80
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-05 23:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
  if ('string' === string.toLowerCase()) {
     console.log("I can\'t hear you!");
 }
