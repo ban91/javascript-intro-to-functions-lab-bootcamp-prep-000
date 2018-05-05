e64f1b0309388fc67fbc4a950a325d477f229262
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-05 23:29:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ function whisper(string) {
 }
 
 function logShout(string) {
- console.log(string.toUpperCase);
+ console.log('hello'.toUpperCase());
 }
 
 function logWhisper(string) {
- console.log(string.toLowerCase);
+ console.log(string.toLowerCase());
 }
 
